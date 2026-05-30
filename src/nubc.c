e7f25d36d7fb0a930ba8652855a6fb7937abe62b
Fernando Dev <ferreirareiscabralfernando@gmail.com>
2026-05-30 16:26:03 -0300

    Improvements to the parser structure and improvements to memory management.

--- src/nubc.c
+++ src/nubc.c
@@ -26,28 +26,34 @@ typedef struct _Lexer Lexer;
 typedef struct _Token Token;
 typedef struct _Node Node;
 
-Arena *GLOBAL_ARENA = null;
-Token *TOKENS = null;
-Node *NODES = null;
+typedef struct _List List;
 
-void arena_free(Arena *arena);
+Arena *GLOBAL_ARENA = null;
+List *GLOBAL_LIST = null;
+Token *TOKENS = null;
+
+static inline void arena_free(Arena *arena);
+static inline void list_free(List *l);
 
 /// ERRORS
 #define ERR_FILE_NOT_FOUND "File not found."
-#define ERR_UNKOWN_TOKEN "Unknown token."
 #define ERR_FILE_EXPECTED "Expected a file as argument."
+#define ERR_UNKOWN_TOKEN "Unknown token."
 #define ERR_OPEN_FILE "Failed to open file."
 #define ERR_MEM_ALLOC "Memory allocation failed."
 #define ERR_BIG_NUM "Number literal too long (max 23 digits)."
 #define ERR_ON_NUM "Invalid number literal."
 #define ERR_ON_DOUBLE "Number literal has multiple decimal points."
-#define ERR_LEXER_OOB "Lexer offset out of bounds."
 #define ERR_AN_ERROR "An error occurred at some stage of the compiler."
 #define ERR_UINT_OVERFLOW "Uint overflow."
 #define ERR_READ_FILE "An error occurred while reading the file."
+
+#define ERR_LEXER_OOB "Lexer offset out of bounds."
 #define ERR_LEXER_INVALID_CHAR "Invalid literal character."
 #define ERR_LEXER_INVALID_CHAR_ESCAPE "Invalid escape character."
 #define ERR_LEXER_INVALID_STRING "Invalid string."
+
+#define ERR_PARSER_OOB "Parser offset out of bounds."
 
 /// UTILS
 typedef struct _NubStr
@@ -75,8 +81,8 @@ void __enforce(int cond, const char *msg, const char *func, const char *file, in
         arena_free(GLOBAL_ARENA);
     if (TOKENS != null)
         free(TOKENS);
-    if (NODES != null)
-        free(NODES);
+    if (GLOBAL_LIST != null)
+        list_free(GLOBAL_LIST);
     printf("An error occurred in the function '%s()' in %s:%d\n", func, file, line);
     printf("Message: %s\n", msg);
     exit(1);
@@ -165,7 +171,7 @@ static inline uint align(uint size, uint base)
     return (size + (base - 1)) & ~(base - 1);
 }
 
-ArenaChunk *chunk_create(uint size)
+static inline ArenaChunk *chunk_create(uint size)
 {
     ArenaChunk *chunk = (ArenaChunk *)calloc(1, sizeof(ArenaChunk));
     enforce(chunk == null, ERR_MEM_ALLOC);
@@ -181,7 +187,7 @@ ArenaChunk *chunk_create(uint size)
     return chunk;
 }
 
-Arena arena_create(uint size)
+static inline Arena arena_create(uint size)
 {
     // alinha o tamanho pra base 8
     size = align(size, 8);
@@ -189,7 +195,7 @@ Arena arena_create(uint size)
     return arena;
 }
 
-void *arena_alloc(Arena *arena, uint size)
+static inline void *arena_alloc(Arena *arena, uint size)
 {
     // alinha o tamanho pra base 8
     size = align(size, 8);
@@ -207,7 +213,7 @@ void *arena_alloc(Arena *arena, uint size)
     return (void *)data;
 }
 
-void arena_free(Arena *arena)
+static inline void arena_free(Arena *arena)
 {
     ArenaChunk *chunk = arena->chunk;
     while (chunk != null)
@@ -217,6 +223,71 @@ void arena_free(Arena *arena)
         free(chunk);
         chunk = next;
     }
+}
+
+/// LIST
+// a lista serve para dados alocados na heap que precisam ser desalocados após resize, arena não pode cobrir isso
+// todos os lists serão salvos no list principal GLOBAL_LIST para serem liberados a fim do programa ou em caso de crash
+#define ELEM_RAW 0  // free normal
+#define ELEM_LIST 1 // list_free recursivo
+#define ELEM_REF 2  // não libera, só referência
+
+typedef struct _list_data
+{
+    void *ptr;
+    int flag;
+} list_data;
+
+typedef struct _List
+{
+    list_data *data;
+    uint size, cap;
+} List;
+
+static inline List *list_create(uint cap)
+{
+    List *l = (List *)calloc(1, sizeof(List));
+    enforce(l == null, ERR_MEM_ALLOC);
+    list_data *data = (list_data *)calloc(1, sizeof(list_data) * cap);
+    enforce(data == null, ERR_MEM_ALLOC);
+    l->cap = cap;
+    l->data = data;
+    l->size = 0;
+    return l;
+}
+
+static inline void list_resize(List *l)
+{
+    uint new_cap = l->cap * 2;
+    list_data *data = (list_data *)realloc(l->data, sizeof(list_data) * new_cap);
+    enforce(data == null, ERR_MEM_ALLOC);
+    l->data = data;
+    l->cap = new_cap;
+}
+
+static inline void list_push(List *l, void *data, int flag)
+{
+    if (l->size == l->cap)
+        list_resize(l);
+    l->data[l->size++] = (list_data){.ptr = data, .flag = flag};
+}
+
+static inline void list_free(List *l)
+{
+    for (uint i = 0; i < l->size; i++)
+        switch (l->data[i].flag)
+        {
+        case ELEM_LIST:
+            list_free((List *)l->data[i].ptr);
+            break;
+        case ELEM_RAW:
+            free(l->data[i].ptr);
+            break;
+        case ELEM_REF:
+            break; // não faz nada
+        }
+    free(l->data);
+    free(l);
 }
 
 /// LEXER
@@ -253,6 +324,8 @@ typedef enum _TokenKind
     TK_SEMICOLON, // ;
     TK_COMMA,     // ,
     TK_DOT,       // .
+    TK_STAR,      // *
+    TK_SLASH,     // /
 
     // eof
     TK_EOF, // só tem um caso de uso
@@ -400,7 +473,7 @@ ubyte lexer_tokenize(Lexer *l)
             switch (str[0])
             {
             case 'f':
-                if (len == 4 && memcmp(str, "func", 4) == 0)
+                if (len == 3 && memcmp(str, "fun", 3) == 0)
                     kind = TK_FN;
                 if (len == 3 && memcmp(str, "for", 3) == 0)
                     kind = TK_FOR;
@@ -599,10 +672,9 @@ ubyte lexer_tokenize(Lexer *l)
             k = TK_MINUS;
             break;
         case '=':
-            if (lexer_peek(l) == '=')
+            k = TK_EQUALS;
+            if (lexer_match(l, '='))
                 k = TK_EEQUALS;
-            else
-                k = TK_EQUALS;
             break;
         case ':':
             k = TK_COLON;
@@ -615,6 +687,12 @@ ubyte lexer_tokenize(Lexer *l)
             break;
         case '.':
             k = TK_DOT;
+            break;
+        case '/':
+            k = TK_SLASH;
+            break;
+        case '*':
+            k = TK_STAR;
             break;
         default:
             break;
@@ -719,6 +797,12 @@ void lexer_debug(Lexer *l)
         case TK_WHILE:
             printf("[WHILE] ");
             continue;
+        case TK_SLASH:
+            printf("[SLASH] ");
+            continue;
+        case TK_STAR:
+            printf("[STAR] ");
+            continue;
         default:
             printf("[DESCONHECIDO] ");
             continue;
@@ -730,8 +814,6 @@ void lexer_debug(Lexer *l)
 /// PARSER
 typedef enum _NodeKind
 {
-    NODE_PROGRAM,
-
     NODE_BINARYEXPR,
     NODE_UNARYEXPR,
 
@@ -769,15 +851,28 @@ typedef struct _NodeReturnStmt
     Node *val;
 } NodeReturnStmt;
 
+typedef struct _NodeFnArg
+{
+    NubStr name;
+    Node *val;
+} NodeFnArg;
+
+typedef struct _NodeFnDecl
+{
+    NubStr name;
+    List *arguments; // NodeFnArg
+    List *body;      // Node*
+} NodeFnDecl;
+
 typedef struct _Node
 {
     NodeKind kind;
     union
     {
-        NodeProgram program;
         NodeBinaryExpr binaryExpr;
         NodeUnaryExpr unaryExpr;
         NodeReturnStmt retStmt;
+        NodeFnDecl fnDecl;
         double doubleLit;
         float floatLit;
         NubStr strLit;
@@ -789,9 +884,289 @@ typedef struct _Node
 
 typedef struct _Parser
 {
-    Node *program;
-    char* source;
+    List *list;
+    uint tk_len, offset;
+    char *source;
 } Parser;
+
+static inline Parser parser_create(char *source, uint len)
+{
+    return (Parser){.source = source, .list = list_create(32), .tk_len = len, .offset = 0};
+}
+
+static inline Node *node_create()
+{
+    Node *node = (Node *)calloc(1, sizeof(Node));
+    enforce(node == null, ERR_MEM_ALLOC);
+    return node;
+}
+
+typedef enum _ParserPrecedence
+{
+    PRE_LOW,
+    PRE_PLUS,
+    PRE_MUL,
+    PRE_HIGH,
+} ParserPrecedence;
+
+// parser_prototipos {{
+ParserPrecedence parser_get_precedence(Token *tk);
+static inline Node *parser_parse_intern(Parser *p);
+// }}
+
+static inline Token *parser_peek(Parser *p)
+{
+    enforce(p->offset == p->tk_len, ERR_PARSER_OOB);
+    return &TOKENS[p->offset];
+}
+
+static inline Token *parser_advance(Parser *p)
+{
+    enforce(p->offset >= p->tk_len, ERR_PARSER_OOB);
+    return &TOKENS[p->offset++];
+}
+
+static inline int parser_match(Parser *p, TokenKind kind)
+{
+    enforce(p->offset == p->tk_len, ERR_PARSER_OOB);
+    if (TOKENS[p->offset].kind == kind)
+    {
+        parser_advance(p);
+        return 1;
+    }
+    return 0;
+}
+
+static inline int parser_check(Parser *p, TokenKind kind)
+{
+    enforce(p->offset == p->tk_len, ERR_PARSER_OOB);
+    return TOKENS[p->offset].kind == kind;
+}
+
+static inline Token *parser_future(Parser *p, uint i)
+{
+    enforce((p->offset + i) >= p->tk_len, ERR_PARSER_OOB);
+    return &TOKENS[p->offset + i];
+}
+
+static inline Token *parser_consume(Parser *p, TokenKind kind, char *message)
+{
+    Token *tk = parser_peek(p);
+    if (tk->kind == kind)
+        return parser_advance(p);
+    nubc_message(ERR_ERROR, message, p->source, tk->pos);
+    enforce(1, message);
+    return tk;
+}
+
+static inline int parser_is_decl(Parser *p)
+{
+    switch (parser_peek(p)->kind)
+    {
+    case TK_FN:
+    case TK_VAR:
+    case TK_CONST:
+        return 1;
+    default:
+        return 0;
+    }
+}
+
+static inline int parser_is_stmt(Parser *p)
+{
+    switch (parser_peek(p)->kind)
+    {
+    case TK_IF:
+    case TK_FOR:
+    case TK_WHILE:
+    case TK_RETURN:
+        return 1;
+    default:
+        return 0;
+    }
+}
+
+Node *parser_parse_decl(Parser *p)
+{
+    Token *tk = parser_peek(p);
+    switch (tk->kind)
+    {
+    case TK_FN:
+    {
+        parser_advance(p); // pula o fun
+        Token *name = parser_consume(p, TK_ID, "Esperado um nome pra função.");
+
+        // TODO: adicionar suporte a argumentos
+        List *args = list_create(8L);
+        list_push(p->list, args, ELEM_LIST);
+
+        parser_consume(p, TK_LPREN, "Esperado '('.");
+        parser_consume(p, TK_RPREN, "Esperado ')' após os argumentos.");
+
+        List *body = list_create(16L);
+        list_push(p->list, body, ELEM_LIST);
+
+        parser_consume(p, TK_LBRACE, "Esperado '{' após os argumentos.");
+
+        while (!parser_check(p, TK_RBRACE))
+            list_push(body, parser_parse_intern(p), ELEM_RAW);
+
+        parser_consume(p, TK_RBRACE, "Esperado '}' após a função.");
+
+        Node *n = node_create();
+        n->kind = NODE_FNDECL;
+        n->value.fnDecl.name = name->str;
+        n->value.fnDecl.arguments = args;
+        n->value.fnDecl.body = body;
+        return n;
+    }
+    default:
+        return null;
+    }
+}
+
+Node *parser_parse_stmt(Parser *p)
+{
+    return null;
+}
+
+// parse_expr {{
+Node *parser_parse_expr_nud(Parser *p)
+{
+    Token *tk = parser_peek(p);
+    switch (tk->kind)
+    {
+    case TK_NUM:
+        parser_advance(p);
+        Node *num = node_create();
+        num->kind = NODE_INT_LIT;
+        num->pos = num->pos;
+        num->value.intLit = tk->num;
+        return num;
+    default:
+        return null;
+    }
+}
+
+Node *parser_parse_expr_led(Parser *p, Node *left)
+{
+    Token *tk = parser_peek(p);
+    switch (tk->kind)
+    {
+    case TK_PLUS:
+    case TK_MINUS:
+    case TK_STAR:
+    case TK_SLASH:
+        return left; // TODO: binary expr
+    default:
+        return left;
+    }
+}
+
+Node *parser_parse_expr(Parser *p, ParserPrecedence precedence)
+{
+    Node *left = parser_parse_expr_nud(p);
+    if (left == null)
+        return null;
+    while (precedence < parser_get_precedence(parser_peek(p)))
+        left = parser_parse_expr_led(p, left);
+    return left;
+}
+// }}
+
+ParserPrecedence parser_get_precedence(Token *tk)
+{
+    switch (tk->kind)
+    {
+    case TK_PLUS:
+    case TK_MINUS:
+        return PRE_PLUS;
+    case TK_STAR:
+    case TK_SLASH:
+        return PRE_MUL;
+    default:
+        return PRE_LOW;
+    }
+}
+
+static inline Node *parser_parse_intern(Parser *p)
+{
+    if (parser_is_decl(p))
+        return parser_parse_decl(p);
+    if (parser_is_stmt(p))
+        return parser_parse_stmt(p);
+    return parser_parse_expr(p, parser_get_precedence(parser_peek(p)));
+}
+
+void parser_parse(Parser *p)
+{
+    List *l = p->list;
+    while (p->offset < p->tk_len)
+    {
+        Node *node = parser_parse_intern(p);
+        if (node == null)
+        {
+            p->offset++;
+            continue;
+        }
+        list_push(l, node, ELEM_RAW);
+    }
+}
+
+static inline void node_debug(Node *n, uint depth)
+{
+    for (uint i = 0; i < depth; i++)
+        printf("  ");
+
+    switch (n->kind)
+    {
+    case NODE_FNDECL:
+        printf("[FNDECL %.*s]\n", n->value.fnDecl.name.len, n->value.fnDecl.name.ptr);
+        if (n->value.fnDecl.body)
+            for (uint i = 0; i < n->value.fnDecl.body->size; i++)
+                node_debug((Node *)n->value.fnDecl.body->data[i].ptr, depth + 1);
+        break;
+    case NODE_RETURNSTMT:
+        printf("[RETURN]\n");
+        if (n->value.retStmt.val)
+            node_debug(n->value.retStmt.val, depth + 1);
+        break;
+    case NODE_BINARYEXPR:
+        printf("[BINARY %d]\n", n->value.binaryExpr.op);
+        node_debug(n->value.binaryExpr.left, depth + 1);
+        node_debug(n->value.binaryExpr.right, depth + 1);
+        break;
+    case NODE_UNARYEXPR:
+        printf("[UNARY %d]\n", n->value.unaryExpr.op);
+        node_debug(n->value.unaryExpr.val, depth + 1);
+        break;
+    case NODE_INT_LIT:
+        printf("[INT %ld]\n", n->value.intLit);
+        break;
+    case NODE_DOUBLE_LIT:
+        printf("[DOUBLE %f]\n", n->value.doubleLit);
+        break;
+    case NODE_FLOAT_LIT:
+        printf("[FLOAT %f]\n", n->value.floatLit);
+        break;
+    case NODE_STR_LIT:
+        printf("[STRING '%.*s']\n", n->value.strLit.len, n->value.strLit.ptr);
+        break;
+    case NODE_CHAR_LIT:
+        printf("[CHAR '%c']\n", n->value.chLit);
+        break;
+    }
+}
+
+static inline void parser_debug(Parser *p)
+{
+    for (uint i = 0; i < p->list->size; i++)
+    {
+        list_data d = p->list->data[i];
+        if (d.flag == ELEM_RAW)
+            node_debug((Node *)d.ptr, 0);
+    }
+}
 
 /// MAIN
 int main(int argc, char **argv)
@@ -827,17 +1202,26 @@ int main(int argc, char **argv)
     // errors
     ubyte err = 0;
 
+    // list
+    List *g_list = list_create(32L);
+    GLOBAL_LIST = g_list;
+
     // lexer
     Lexer lexer = lexer_create(filename, file_size, source);
     err = lexer_tokenize(&lexer);
     enforce(err > 0, ERR_AN_ERROR);
     lexer_debug(&lexer);
 
+    // parser
+    Parser parser = parser_create(source, lexer.tk_size);
+    list_push(GLOBAL_LIST, parser.list, ELEM_LIST);
+    parser_parse(&parser);
+    parser_debug(&parser);
+
     // end
-    if (TOKENS != null)
-        free(TOKENS);
-    if (NODES != null)
-        free(NODES);
+    free(TOKENS);
+    list_free(GLOBAL_LIST);
     arena_free(GLOBAL_ARENA);
+
     return 0;
 }
